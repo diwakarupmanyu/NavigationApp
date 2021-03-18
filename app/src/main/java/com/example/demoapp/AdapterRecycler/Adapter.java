fd041a2e596d1bfa6686a6d1926b1007b2dc8301
diwakarupmanyu <diwakarupmanyu@gmail.com>
2021-03-18 15:37:25 +0530

    commit

---
+++ app/src/main/java/com/example/demoapp/AdapterRecycler/Adapter.java
@@ -0,0 +1,86 @@
+package com.example.demoapp.AdapterRecycler;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.demoapp.Activity.Detail_Activity;
+import com.example.demoapp.Model.Model;
+import com.example.demoapp.R;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>
+
+{
+    ArrayList<Model> data;
+
+    public Adapter(Object list_Of_Person)
+    {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view= layoutInflater.inflate(R.layout.listitem,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        final Model temp = data.get(position);
+
+        holder.name.setText(data.get(position).getName());
+        holder.branch.setText(data.get(position).getBranch());
+        holder.imageView.setImageResource(data.get(position).getImage());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if(view.getContext() !=null) {
+                    Intent intent = new Intent(view.getContext(), Detail_Activity.class);
+                    intent.putExtra("name", temp.getName());
+                    intent.putExtra("branch", temp.getBranch());
+                    intent.putExtra("image", temp.getImage());
+                    view.getContext().startActivity(intent);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return data.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name,branch;
+
+        public ViewHolder( View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            name = itemView.findViewById(R.id.name);
+            branch = itemView.findViewById(R.id.branch);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/demoapp/PersonList/List_Of_Persons.java
@@ -0,0 +1,17 @@
+package com.example.demoapp.PersonList;
+
+import android.view.View;
+
+import com.example.demoapp.AdapterRecycler.Adapter;
+import com.example.demoapp.Model.Model;
+import com.example.demoapp.R;
+
+import java.util.ArrayList;
+
+public class List_Of_Persons
+{
+
+}
+
+
+
